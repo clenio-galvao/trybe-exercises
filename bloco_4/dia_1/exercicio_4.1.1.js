c61d8833906619d118a2b009464c94dd90705e3e
Clênio Galvão <clenio.cgm@gmail.com>
2020-10-23 18:30:39 -0300

    modifica exercício 1 e cria o 2

---
+++ bloco_4/dia_1/exercicio_4.1.1.js
@@ -0,0 +1,21 @@
+//Exercício 1: Faça cinco programas, um para cada operação aritmética básica. Seu programa deve ter duas variáveis, a e b, definidas no começo com os valores que serão operados. Faça programas para: Adição Subtração Multiplicação Divisão e Módulo
+
+let a = 8;
+let b = 4;
+
+//programa1 soma
+soma = a + b; 
+console.log("a soma dos números é: " + soma);
+//programa2 subtração
+subtracao = a - b;
+console.log("a subtração dos números é: " + subtracao);
+//programa3 multiplicação
+multiplicacao = a * b;
+console.log("a multiplicação dos números é: " + multiplicacao);
+//programa4
+divisao = a / b;
+console.log("a divisão dos números é: " + divisao);
+//programa5
+resto = a % b;
+console.log("existe resto na divisão dos números (1-sim e 0-não): " + resto);
+//Fim exercício 1
---
+++ bloco_4/dia_1/exercicio_4.1.2.js
@@ -0,0 +1,12 @@
+//Exercício 2: Faça um programa que retorne o maior de dois números. Defina no começo do programa duas variáveis com os valores que serão comparados.
+
+let a = 1;
+let b = 2;
+
+if (a > b) {
+    maior = a;
+} else {
+    maior = b;
+}
+console.log(maior)
+//Fim exercício 2
