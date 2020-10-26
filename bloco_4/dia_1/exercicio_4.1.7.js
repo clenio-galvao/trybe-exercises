bb77f7fea76baf6223fde92f9cbe104b053f7385
Clênio Galvão <clenio.cgm@gmail.com>
2020-10-26 16:17:39 -0300

    Modifica 7 e insere 8, 9, 10 e 11

--- bloco_4/dia_1/exercicio_4.1.7.js
+++ bloco_4/dia_1/exercicio_4.1.7.js
@@ -7,27 +7,31 @@ Porcentagem >= 50 -> E
 Porcentagem < 50 -> F
 O programa deve retornar uma mensagem de erro e encerrar se a nota passada for menor que 0 ou maior que 100.*/
 
-nota = 60;
-if (nota>=90) {
+nota = 100;
+if (nota>=90 && nota<=100) {
     nota = "A";
-    console.log("Sua nota foi: " + nota);
+    console.log("Sua nota é: " + nota);
 }
 else if (nota>=80 && nota<90) {
     nota = "B";
-    console.log("Sua nota foi: " + nota);
+    console.log("Sua nota é: " + nota);
 }
 else if (nota>=70 && nota<80) {
     nota = "C";
-    console.log("Sua nota foi: " + nota);
+    console.log("Sua nota é: " + nota);
 }
 else if (nota>=60 && nota<70) {
     nota = "D";
-    console.log("Sua nota foi: " + nota);
+    console.log("Sua nota é: " + nota);
 }
 else if (nota>=50 && nota<60) {
     nota = "E";
-    console.log("Sua nota foi: " + nota);
+    console.log("Sua nota é: " + nota);
+}
+else if (nota<50) {
+    nota = "F";
+    console.log("Sua nota é: " + nota);
 }
 else {
-    console.log("Sua nota foi: " + nota + " - estude mais");
+    console.log("Sua nota está fora dos parâmetros máximos ou mínimos, lembre-se: não existe nota menor que 0 ou maior que 100");
 }
---
+++ bloco_4/dia_1/exercicio_4.1.9.js
@@ -0,0 +1,13 @@
+/* Exercício 9: Escreva um programa que defina três números em variáveis no seu começo e retorne true se pelo menos uma das três for ímpar. Caso contrário, ele retorna false.
+Bonus: use somente um if. */
+
+a = 2;
+b = 4;
+c = 2;
+
+if (a%2==1 || b%2==1 || c%2==1) {
+    console.log("true");
+}
+else {
+    console.log("false");
+}
