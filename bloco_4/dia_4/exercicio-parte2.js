693dabae1b912872b0009f66a2d413ca96d829ea
Clênio Galvão <clenio.cgm@gmail.com>
2020-10-29 14:26:04 -0300

    resolve exercicio 5 modulo 2

--- bloco_4/dia_4/exercicio-parte2.js
+++ bloco_4/dia_4/exercicio-parte2.js
@@ -3,8 +3,8 @@ Exemplo de palíndromo: arara.
 verificaPalindrome("arara");
 Retorno esperado: true
 verificaPalindrome("desenvolvimento");
-Retorno esperado: false*/
-/*
+Retorno esperado: false
+
 function verificaPalindrome(palavra){
     let ehPalin = false;
     let palavraInvertida = "";
@@ -19,12 +19,12 @@ function verificaPalindrome(palavra){
 console.log(verificaPalindrome('ananananana'));
 console.log(verificaPalindrome('desenvolvimento'));
 
-/*Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
 Array de teste: [2, 3, 6, 7, 10, 1];.
-Valor esperado no retorno da função: 4.*/
+Valor esperado no retorno da função: 4.
 
-// let arrayTeste = [2, 3, 6, 7, 10, 1];
-/*
+// let arrayTeste = [2, 3, 6, 7, 10, 1];*/
+
 function maior (array) {
     let aux = array[0];
     for (index in array) {
@@ -34,14 +34,14 @@ function maior (array) {
     }
     return aux;   
 }
-console.log(maior(arrayTeste));
+/*console.log(maior(arrayTeste));
 
-/*Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
 Array de teste: [2, 4, 6, 7, 10, 0, -3];.
-Valor esperado no retorno da função: 6.*/
+Valor esperado no retorno da função: 6.
 
 // arrayTeste = [2, 4, 6, 7, 10, 0, -3];
-/*
+
 function menor (array) {
     let aux = array[0];
     for (index in array) {
@@ -53,11 +53,11 @@ function menor (array) {
 }
 console.log(menor(arrayTeste));
 
-/*Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
+Crie uma função que receba um array de nomes e retorne o nome com a maior quantidade de caracteres.
 Array de teste: ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];.
-Valor esperado no retorno da função: Fernanda.*/
+Valor esperado no retorno da função: Fernanda.
 
-arrayTeste = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
+//let arrayTeste = ['José', 'Lucas', 'Nádia', 'Fernanda', 'Cairo', 'Joana'];
 
 function maiorString (array) {
     let nomeMaior = "";
@@ -71,3 +71,33 @@ function maiorString (array) {
     return nomeMaior;   
 }
 console.log(maiorString(arrayTeste));
+
+Crie uma função que receba um array de inteiros e retorne o inteiro que mais se repete.
+Array de teste: [2, 3, 2, 5, 8, 2, 3];.
+Valor esperado no retorno da função: 2.*/
+
+let arrayTeste = [2, 3, 2, 5, 8, 2, 3];
+
+
+function numRepeteMais(array){
+    let repeticao = [];
+    let repeteMais = 0;
+    let aux = 0;    
+    for (let pos = 0; pos < array.length; pos +=1) {
+        let rep = 0;
+        let elemento = array[pos];         
+        for (let index = 0; index < array.length; index +=1) {
+            if (array[index] == elemento) {
+                rep = rep + 1;           
+            }            
+        }
+        if (rep > aux) {
+            repeteMais = array[pos];
+            aux = rep;
+        }               
+    } 
+    return repeteMais;
+}
+console.log(numRepeteMais(arrayTeste));
+
+
