84ee4c48c32578da6c6b228c7cb4943a6ecd6578
Clênio Galvão <clenio.cgm@gmail.com>
2020-11-25 18:20:04 -0300

    resolve exercicio 3

--- bloco_7/dia_3/script.js
+++ bloco_7/dia_3/script.js
@@ -14,7 +14,13 @@ sum (4, 5);
 
 assert.strictEqual(sum(4, 5), 9, "resultado esperado 9"); 
 assert.strictEqual(sum(0, 0), 0, "resultado esperado 0"); 
-assert.strictEqual(sum(4, 5), 9, "resultado esperado 9"); 
+assert.strictEqual(sum(4, 5), 9, "resultado esperado 9");
+assert.throws(() => {
+  sum(4, '5');
+});
+assert.throws(() => {
+  sum(4, '5');
+}, /^Error: parameters must be numbers$/); 
 
 function myRemove(arr, item) {
   let newArr = [];
@@ -28,10 +34,30 @@ function myRemove(arr, item) {
 const arrayNovo = myRemove([1, 2, 3, 4], 3); 
 const arrayNovo2 = myRemove([1, 2, 3, 4], 5);
 // implemente seus testes aqui
-// assert.strictEqual(arrayNovo, [1, 2, 4], 'remover o número 3 do array');   // erro, deveria ocorrer o contrário
+assert.deepStrictEqual(arrayNovo, [1, 2, 4]);   // pq tem que colocar o deep??
 assert.notStrictEqual(arrayNovo, [1, 2, 3, 4], 'arrayNovo não pode ser o antigo')
-// assert.deepStrictEqual(arrayNovo, [1, 2, 3, 4], 'teve mudanças no array passado como parâmetro')
-// assert.strictEqual(arrayNovo2, [1, 2, 3, 4], 'o array não tem o número 5, não deveria ter erro') // erro, deveria ocorrer o contrário
+// assert.deepStrictEqual(arrayNovo, [1, 2, 3, 4], 'teve mudanças no array passado como parâmetro') // passou mas precisa ficar comentada
+assert.deepStrictEqual(arrayNovo2, [1, 2, 3, 4], 'o array não tem o número 5, não deveria ter erro') // pq tem que colocar o deep?
 
 
+function myRemoveWithoutCopy(arr, item) {
+  for (let i = 0, len = arr.length; i < len; i += 1) {
+    if (arr[i] === item) {
+      arr.splice(i, 1);
+      i -= 1;
+      len -= 1;
+    }
+  }
+
+  return arr;
+}
+const arrayNovo3 = myRemoveWithoutCopy([1, 2, 3, 4], 3); 
+const arrayNovo4 = myRemoveWithoutCopy([1, 2, 3, 4], 5);
+
+// implemente seus testes aqui
+
+assert.deepStrictEqual(arrayNovo3, [1, 2, 4], 'não removeu o número 3 do array?');
+assert.notStrictEqual(arrayNovo3, [1, 2, 3, 4], 'arrayNovo não pode ser igual o antigo');
+// assert.deepStrictEqual(arrayNovo3, [1, 2, 3, 4], 'teve mudanças no array passado como parâmetro');
+assert.deepStrictEqual(arrayNovo4, [1, 2, 3, 4], 'o array não tem o número 5, não deveria ter erro');
 
