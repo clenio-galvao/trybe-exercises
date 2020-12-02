161acd870bdb893ed025090b11a82adfa7697350
Clênio Galvão <clenio.cgm@gmail.com>
2020-12-02 18:05:30 -0300

    resolve exercicio 2

--- bloco_8/dia_3/exercicios.js
+++ bloco_8/dia_3/exercicios.js
@@ -1,4 +1,4 @@
-
+// 1. Dada uma matriz de matrizes, transforme em uma única matriz.
 const assert = require('assert');
 
 const arrays = [
@@ -16,3 +16,84 @@ function flatten() {
 }
 
 assert.deepStrictEqual(flatten(), ["1", "2", "3", true, 4, 5, 6]);
+
+// 2. Crie uma string com os nomes de todas as pessoas autoras.
+
+const books = [
+  {
+    id: 1,
+    name: 'As Crônicas de Gelo e Fogo',
+    genre: 'Fantasia',
+    author: {
+      name: 'George R. R. Martin',
+      birthYear: 1948
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: 'O Senhor dos Anéis',
+    genre: 'Fantasia',
+    author: {
+      name: 'J. R. R. Tolkien',
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: 'Fundação',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Isaac Asimov',
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: 'Duna',
+    genre: 'Ficção Científica',
+    author: {
+      name: 'Frank Herbert',
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: 'A Coisa',
+    genre: 'Terror',
+    author: {
+      name: 'Stephen King',
+      birthYear: 1947
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: 'O Chamado de Cthulhu',
+    genre: 'Terror',
+    author: {
+      name: 'H. P. Lovecraft',
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+
+function allNames() {
+  const authorNames = books.reduce((acc, element, index) => {
+    if (index === books.length - 1) {
+      return `${acc} ${element.author.name}.`;
+    } else {
+      return `${acc} ${element.author.name},`;
+    }
+  }, '');
+  return `Nomes:${authorNames}`;
+}
+
+assert.deepStrictEqual(allNames(), "Nomes: George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.");
+
+//
