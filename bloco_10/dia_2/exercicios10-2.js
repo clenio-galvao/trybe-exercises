ab2702d9cabb255b3dbf3ad17ccc19a0a2ae2797
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-05 19:58:23 -0300

    resolve 3, 4, 5 e 6

--- bloco_10/dia_2/exercicios10-2.js
+++ bloco_10/dia_2/exercicios10-2.js
@@ -21,6 +21,37 @@ const getUserName = (userID) => {
   return findUserById(userID).then(user => user.name);
 }
 
-console.log(findUserById(3));
+const fetch = require('node-fetch');
 
-module.exports = {uppercase, findUserById, getUserName};
+const getRepos = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      return data.map((repo) => repo.name)
+    });
+}
+
+const Animals = [
+  { name: 'Dorminhoco', age: 1, type: 'Dog' },
+  { name: 'Soneca', age: 2, type: 'Dog' },
+  { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalsByName = (name) => (
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const arrayAnimals = Animals.find((animal) => animal.name === name);
+      if (arrayAnimals) {
+        return resolve(arrayAnimals);
+      }
+
+      return reject({ error: 'Nenhum animal com esse nome!' });
+    }, 100);
+  })
+);
+
+const getAnimal = (name) => (
+  findAnimalsByName(name).then(list => list)
+);
+
+module.exports = {uppercase, findUserById, getUserName, getRepos, findAnimalsByName, getAnimal};
--- bloco_10/dia_2/exercicios10-2.test.js
+++ bloco_10/dia_2/exercicios10-2.test.js
@@ -1,4 +1,4 @@
-const {uppercase, findUserById, getUserName} = require('./exercicios10-2');
+const {uppercase, getUserName, getRepos, getAnimal} = require('./exercicios10-2');
 
 test('se a callback de função uppercase, que transforma as letras de uma palavra em letras maiúsculas', () => {
   const callback = (data) => {
@@ -18,5 +18,57 @@ test ('se o resultado da função getUserName para o caso em que o usuário não
   expect.assertions(1)
   return getUserName(3).catch(error => {
     expect(error).toEqual({ error: 'User with 3 not found.' });
+  });
+});
+
+test ('se o resultado da função getUserName para o caso em que o usuário é encontrado usando async/await', async () => {
+  const name = await getUserName(4);
+  expect(name).toBe('Mark');
+});
+
+test ('se o resultado da função getUserName para o caso em que o usuário não é encontrado usando async/await', async () => {
+  try {
+    await getUserName(3);
+  } catch (error) {
+    expect(error).toEqual({ error: 'User with 3 not found.' });
+  }
+});
+
+test('Dada a URL https://api.github.com/users/tryber/repos, verifique que os repositórios challenge-bug-hunting-youtrybe-squad-3 e iss-location se encontram nessa lista.', async () => {
+  const RepoNames = await getRepos('https://api.github.com/users/tryber/repos');
+  expect.assertions(2);
+  expect(RepoNames).toContain('challenge-bug-hunting-youtrybe-squad-3');
+  expect(RepoNames).toContain('iss-location');
+});
+
+// beforeEach(() => console.log('1 - beforeEach'));
+// afterEach(() => console.log('1 - afterEach'));
+
+// test('', () => console.log('1 - test'));
+
+// describe('Scoped / Nested block', () => {
+//   beforeEach(() => console.log('2 - beforeEach'));
+//   afterEach(() => console.log('2 - afterEach'));
+
+//   test('', () => console.log('2 - test'));
+// });
+
+describe('Testando promise - findAnimalByName', () => {
+  describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+      expect.assertions(1);
+      return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+      });
+    });
+  });
+
+  describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+      expect.assertions(1);
+      return getAnimal('Bob').catch(error => {
+        expect(error).toEqual({ error: 'Nenhum animal com esse nome!' })
+      });
+    });
   });
 });
