e591822f518dcbcb132809f79169b41382411f3a
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-06 19:41:36 -0300

    Inicia exercicio 5

--- bloco_10/dia_3/exercices10-3.js
+++ bloco_10/dia_3/exercices10-3.js
@@ -12,6 +12,7 @@ const stringUp = (str) => str.toUpperCase();
 const stringFrst = (str) => str[0];
 const stringConcat = (str1, str2) => str1.concat(str2);
 
+// 5- Utilizando as mesmas funções do exercício anterior, repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar.
 
 module.exports = {
   randomNumber,
--- bloco_10/dia_3/exercices10-3.test.js
+++ bloco_10/dia_3/exercices10-3.test.js
@@ -2,79 +2,107 @@
 
 let functions = require('./exercices10-3');
 
-jest.mock('./exercices10-3');
+// jest.mock('./exercices10-3'); não pode usar quando quiser utilizar a função original
 
-afterEach(functions.randomNumber.mockReset);
+describe('resolve exercício 1', () => {
+  it('resolve exercício 1', () => {
+    functions.randomNumber = jest.fn()
+      .mockReturnValue(10);
 
-test('resolve exercício 1', () => {
-  functions.randomNumber.mockReturnValue(10);
-
-  expect(functions.randomNumber).toHaveBeenCalledTimes(0);
-  functions.randomNumber();
-  expect(functions.randomNumber).toHaveBeenCalled();
-  expect(functions.randomNumber).toHaveBeenCalledTimes(1);
-  expect(functions.randomNumber()).toBe(10);
-  expect(functions.randomNumber).toHaveBeenCalledTimes(2);
-});
-
+    expect(functions.randomNumber).toHaveBeenCalledTimes(0);
+    functions.randomNumber();
+    expect(functions.randomNumber).toHaveBeenCalled();
+    expect(functions.randomNumber).toHaveBeenCalledTimes(1);
+    expect(functions.randomNumber()).toBe(10);
+    expect(functions.randomNumber).toHaveBeenCalledTimes(2);
+  });
+})
 // 2- Com a mesma função do exercício anterior, utilizando o mock, crie uma nova implementação, que deve receber dois parâmetros e retornar a divisão do primeiro pelo segundo. Essa implementação deve ocorrer uma única vez. Faça os testes necessários.
 
-test('resolve exercício 2', () => {
-  functions.randomNumber.mockImplementationOnce((a, b) => a / b)
-    .mockReturnValue(10);
+describe('resolve exercício 2', () => {
+  test('resolve exercício 2', () => {
+    functions.randomNumber = jest.fn().mockImplementationOnce((a, b) => a / b)
+      .mockReturnValue(10);
 
-  expect(functions.randomNumber).toHaveBeenCalledTimes(0);
-  expect(functions.randomNumber(4, 2)).toBe(2);
-  expect(functions.randomNumber).toHaveBeenCalled();
-  expect(functions.randomNumber).toHaveBeenCalledTimes(1);
+    expect(functions.randomNumber).toHaveBeenCalledTimes(0);
+    expect(functions.randomNumber(4, 2)).toBe(2);
+    expect(functions.randomNumber).toHaveBeenCalled();
+    expect(functions.randomNumber).toHaveBeenCalledTimes(1);
 
-  expect(functions.randomNumber()).toBe(10);
-  expect(functions.randomNumber).toHaveBeenCalledTimes(2);
+    expect(functions.randomNumber()).toBe(10);
+    expect(functions.randomNumber).toHaveBeenCalledTimes(2);
+  });
 });
 
 // 3- Ainda com a mesma função do primeiro exercício, utilizando o mock, crie uma nova implementação que receba três parâmetros e retorne sua multiplicação. Após fazer os devidos testes para ela, resete sua implementação e crie uma nova, que receba um parâmetro e retorne seu dobro. Faça os testes necessários.
 
-test('resolve exercício 3', () => {
-  functions.randomNumber.mockImplementation((a, b, c) => a * b * c);
+describe('resolve exercício 3', () => {
+  test('resolve exercício 3', () => {
+    functions.randomNumber = jest.fn().mockImplementation((a, b, c) => a * b * c);
 
-  expect(functions.randomNumber).toHaveBeenCalledTimes(0);
-  expect(functions.randomNumber(1, 2, 3)).toBe(6);
-  expect(functions.randomNumber).toHaveBeenCalled();
-  expect(functions.randomNumber).toHaveBeenCalledTimes(1);
-  expect(functions.randomNumber(2, 2, 3)).toBe(12);
-  expect(functions.randomNumber).toHaveBeenCalled();
-  expect(functions.randomNumber).toHaveBeenCalledTimes(2);
+    expect(functions.randomNumber).toHaveBeenCalledTimes(0);
+    expect(functions.randomNumber(1, 2, 3)).toBe(6);
+    expect(functions.randomNumber).toHaveBeenCalled();
+    expect(functions.randomNumber).toHaveBeenCalledTimes(1);
+    expect(functions.randomNumber(2, 2, 3)).toBe(12);
+    expect(functions.randomNumber).toHaveBeenCalled();
+    expect(functions.randomNumber).toHaveBeenCalledTimes(2);
 
-  functions.randomNumber.mockReset()
-    .mockImplementation(a => a * a);
+    functions.randomNumber.mockReset()
+      .mockImplementation(a => a * a);
 
-  expect(functions.randomNumber(2)).toBe(4);
-  expect(functions.randomNumber).toHaveBeenCalled();
-  expect(functions.randomNumber).toHaveBeenCalledTimes(1);
+    expect(functions.randomNumber(2)).toBe(4);
+    expect(functions.randomNumber).toHaveBeenCalled();
+    expect(functions.randomNumber).toHaveBeenCalledTimes(1);
+  });
 });
-
 // 4- Dentro de um mesmo arquivo, crie três funções. A primeira deve receber uma string e retorná-la em caixa alta. A segunda deve também receber uma string e retornar só a primeira letra. A terceira deve receber duas strings e concatená-las. Faça o mock do arquivo. Faça uma nova implementação para a primeira função, mas agora ela deve retornar a string em caixa baixa. Para a segunda função, uma nova implementação deve retornar a última letra de uma string. A terceira deve receber três strings e concatená-las.
 
-test('resolve exercício 4', () => {
-  functions.stringUp.mockImplementationOnce(str => str.toLowerCase());
-  functions.stringFrst.mockImplementationOnce((str) => {
-    const len = str.length;
-    return str[len - 1];
+describe('resolve exercício 4', () => {
+  test('resolve exercício 4', () => {
+    functions.stringUp = jest.fn().mockImplementation(str => str.toLowerCase());
+    functions.stringFrst = jest.fn().mockImplementation((str) => {
+      const len = str.length;
+      return str[len - 1];
+    });
+    functions.stringConcat = jest.fn().mockImplementation((str1, str2, str3) => str1.concat(str2, str3));
+
+    expect(functions.stringUp).toHaveBeenCalledTimes(0);
+    expect(functions.stringUp('AAAA')).toBe('aaaa');
+    expect(functions.stringUp).toHaveBeenCalled();
+    expect(functions.stringUp).toHaveBeenCalledTimes(1);
+
+    expect(functions.stringFrst).toHaveBeenCalledTimes(0);
+    expect(functions.stringFrst('cabeça')).toBe('a');
+    expect(functions.stringFrst).toHaveBeenCalled();
+    expect(functions.stringFrst).toHaveBeenCalledTimes(1);
+
+    expect(functions.stringConcat).toHaveBeenCalledTimes(0);
+    expect(functions.stringConcat('cabeça ', 'olho ', 'orelha')).toBe('cabeça olho orelha');
+    expect(functions.stringConcat).toHaveBeenCalled();
+    expect(functions.stringConcat).toHaveBeenCalledTimes(1);
   });
-  functions.stringConcat.mockImplementationOnce((str1, str2, str3) => str1.concat(str2, str3));
+});
 
-  expect(functions.stringUp).toHaveBeenCalledTimes(0);
-  expect(functions.stringUp('AAAA')).toBe('aaaa');
-  expect(functions.stringUp).toHaveBeenCalled();
-  expect(functions.stringUp).toHaveBeenCalledTimes(1);
+// 5- Utilizando as mesmas funções do exercício anterior, repita a implementação para a primeira função. Após repetir a implementação, restaure a implementação original e crie os testes necessários para validar.
 
-  expect(functions.stringFrst).toHaveBeenCalledTimes(0);
-  expect(functions.stringFrst('cabeça')).toBe('a');
-  expect(functions.stringFrst).toHaveBeenCalled();
-  expect(functions.stringFrst).toHaveBeenCalledTimes(1);
+describe('resolve exercício 5', () => {
+  test('resolve exercício 5', () => {
+    functions.stringUp.mockReset();
+    console.log(functions.stringUp('aaaa'));
+    let mockStringUp = jest.spyOn(functions, 'stringUp');
+    console.log(mockStringUp('aaaa'));
 
-  expect(functions.stringConcat).toHaveBeenCalledTimes(0);
-  expect(functions.stringConcat('cabeça ', 'olho ', 'orelha')).toBe('cabeça olho orelha');
-  expect(functions.stringConcat).toHaveBeenCalled();
-  expect(functions.stringConcat).toHaveBeenCalledTimes(1);
+    expect(mockStringUp).toHaveBeenCalledTimes(2);
+    expect(mockStringUp('AAAA')).toBe('aaaa');
+    expect(mockStringUp).toHaveBeenCalled();
+    expect(mockStringUp).toHaveBeenCalledTimes(3);
+
+    mockStringUp.mockRestore();
+
+    expect(mockStringUp).toHaveBeenCalledTimes(1);
+    expect(mockStringUp('aaaa')).toBe('AAAA');
+    expect(mockStringUp).toHaveBeenCalled();
+    expect(mockStringUp).toHaveBeenCalledTimes(2);
+  });
 });
