699388b42c33baec0650d13333896816870b0c5a
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-18 15:27:19 -0300

    termina bonus

--- bloco_12/dia_1/my-pokedex/src/Button.js
+++ bloco_12/dia_1/my-pokedex/src/Button.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class Button extends React.Component {
   render() {
-    return <button onClick={this.props.onClick} name={this.props.type}>{this.props.type}</button>;
+    return <button onClick={this.props.onClick} name={this.props.type} >{this.props.type}</button>;
   }
 }
 
--- bloco_12/dia_1/my-pokedex/src/pokedex.js
+++ bloco_12/dia_1/my-pokedex/src/pokedex.js
@@ -12,10 +12,12 @@ class Pokedex extends React.Component {
     this.botaoPassar = this.botaoPassar.bind(this);
     this.botaoAll = this.botaoAll.bind(this);
     this.botaoFiltrar = this.botaoFiltrar.bind(this);
+    this.filtrarClasses = this.filtrarClasses.bind(this);
 
     this.state = {
       pokemonInd: 0,
       filtredPokemons: pokemons,
+      botaoOn: false,
     }
   }
 
@@ -23,8 +25,9 @@ class Pokedex extends React.Component {
     this.setState(
       {
         pokemonInd: 0,
-        filtredPokemons: pokemons
-    });
+        filtredPokemons: pokemons,
+        botaoOn: false,
+      });
   }
 
   botaoFiltrar(event) {
@@ -35,43 +38,42 @@ class Pokedex extends React.Component {
         filtredPokemons: pokemonsFiltered,
       }
     ));
+    if (pokemonsFiltered.length === 1) {
+      this.setState({botaoOn: true});
+    } else {
+      this.setState({botaoOn: false});
+    }
   }
 
   filtrarClasses() {
     const pokTypes = pokemons.map(element => element.type)
-    .sort()
-    .map((ele, index) => {
-      if (index > 0) {
-       if (ele !== ele[index-1]) {
-        return ele
-       }
-      }
-      return ele;      
-    });
-    console.log(pokTypes)
-    return pokTypes   
+      .reduce((unico, item) => {
+        return unico.includes(item) ? unico : [...unico, item]
+      }, []);
+    return pokTypes;
   }
+
 
   botaoPassar() {
-    this.setState((anterior, _props) => this.state.filtredPokemons.length !== anterior.pokemonInd + 1 ? ({pokemonInd: anterior.pokemonInd + 1}) : ({pokemonInd: 0}));
+    this.setState((anterior, _props) => this.state.filtredPokemons.length !== anterior.pokemonInd + 1 ? ({ pokemonInd: anterior.pokemonInd + 1 }) : ({ pokemonInd: 0 }));
   }
 
-  render() {
-   
-    return (
-      <div>
-        <div className="pokedex">
-          <Pokemon pokemonGood={this.state.filtredPokemons[this.state.pokemonInd]} />
-        </div>
-        <div>
-          <button onClick={this.botaoAll}>All</button>
-          {this.filtrarClasses().map(elem => <Button onClick={this.botaoFiltrar} type={elem}/>)}
-        </div>
-        <button onClick={this.botaoPassar}>Mude o Pokemon</button>
+render() {
+
+  return (
+    <div>
+      <div className="pokedex">
+        <Pokemon pokemonGood={this.state.filtredPokemons[this.state.pokemonInd]} />
       </div>
-      
-    );
-  }
+      <div>
+        <button onClick={this.botaoAll}>All</button>
+        {this.filtrarClasses().map(elem => <Button onClick={this.botaoFiltrar} type={elem} disabled={false}/>)}
+      </div>
+      <button className="mudaPok" onClick={this.botaoPassar} disabled={this.state.botaoOn}>Mude o Pokemon</button>
+    </div>
+
+  );
+}
 }
 
 export default Pokedex;
