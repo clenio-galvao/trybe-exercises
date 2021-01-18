d4938d342bcedddda36aa9cd789601e10d69808c
Clênio Galvão <clenio.cgm@gmail.com>
2021-01-18 16:27:18 -0300

    Inicia exercicios 12.2 e resolve 1

---
+++ bloco_12/dia_2/my-form-2.0/src/App.js
@@ -0,0 +1,10 @@
+import FormularioCadastro from './formulario-cadastro.jsx';
+import './App.css';
+
+function App() {
+  return (
+    <FormularioCadastro />
+  );
+}
+
+export default App;
---
+++ bloco_12/dia_2/my-form-2.0/src/formulario-cadastro.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class FormularioCadastro extends React.Component {
+  constructor() {
+    super()
+
+    this.alterarAoDigitar = this.alterarAoDigitar.bind(this);
+
+    this.state = {
+      nome: '',
+    }
+  }
+
+  alterarAoDigitar (event) {
+    this.setState({[event.target.name]: event.target.value});
+    if (event.target.name === 'nome') {
+      this.setState({nome: event.target.value.toUpperCase()});
+    }
+  }
+
+  render() {
+    return (
+    <form>
+      <h1>Preencha o formulário abaixo:</h1>
+      <label>
+        <div>Nome</div> 
+        <input type="text" name="nome" placeholder="Nome completo" value={this.state.nome} onChange={this.alterarAoDigitar} maxLength={40} required/>
+      </label>
+    </form>
+    );
+  }
+}
+
+export default FormularioCadastro;
